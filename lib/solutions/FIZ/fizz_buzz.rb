dc03d941b456d6777df643c9f24cc03d20d9a67c
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 11:10:43 +0000

    Mon Apr 08 11:10:43 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,6 +1,21 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
 
+  @fizz = false
+  @buzz = false
+
+  def return_what(number)
+    if @fizz == true && @ buzz == true
+      return "fizz buzz"
+    elsif @fizz == true
+      return "fizz"
+    elsif @buzz == true
+      return "buzz"
+    else
+      return number
+    end
+  end
+
   def fizz_buzz(number)
     if number % 3 == 0 && number % 5 == 0
       return "fizz buzz"
@@ -14,3 +29,4 @@ class FizzBuzz
   end
 
 end
+
